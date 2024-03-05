ab554675c7f727c04c1beaf7e253c559675d9cb8
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2024-03-05 15:43:39 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# GDG Portlaoise Website
+
+This is the repo containing the code for the official website of Google Developer Groups Portlaoise.
+
+## Tech Stack
+
+This website is built using [SvelteKit](https://kit.svelte.dev/), a framework for building web applications with Svelte. It uses [Tailwind CSS](https://tailwindcss.com/) and [DaisyUI](https://daisyui.com) for styling.
+
+## Getting Started
+
+Once you've pulled this repo, you can start the development server by running the following commands:
+
+```bash
+npm install
+
+npm run dev
+
+# or start the server and open the app in a new browser tab
+npm run dev -- --open
+```
+
+## Building
+
+To create a production version of your app:
+
+```bash
+npm run build
+```
+
+You can preview the production build with `npm run preview`.
+
+## Deployment
+
+This website is deployed using GitHub Pages. To deploy the website, simply push your changes to the `main` branch and the website will be automatically deployed.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,87 @@
+<div class="hero min-h-[60vh]" style="background-image: url(/images/IMG_1739.jpg);">
+	<div class="hero-overlay bg-opacity-60"></div>
+	<div class="hero-content text-center text-neutral-content">
+		<div class="max-w-2xl">
+			<h1 class="mb-5 text-5xl font-bold">Welcome to GDG Portlaoise</h1>
+			<p class="mb-5 text-xl">Your Midlands Hub for Tech Innovation, Learning, & Community</p>
+			<a class="btn btn-primary" href="https://gdg.community.dev/gdg-portlaoise/" target="_blank"
+				>Join our Community</a
+			>
+		</div>
+	</div>
+</div>
+
+<div class="container mx-auto">
+	<div class="grid grid-cols-1 lg:grid-cols-2 py-20 px-10 gap-5">
+		<h2 class="text-4xl font-bold">Powering Tech Growth in the Midlands</h2>
+		<div>
+			<p class="text-lg">
+				We're GDG Portlaoise, your local chapter of the global Google Developer Group network. We're
+				dedicated to building a community to connect tech enthusiasts across the Midlands, providing
+				a dynamic space to learn, collaborate, and push boundaries.
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="container mx-auto p-10">
+	<h2 class="text-3xl font-bold mb-5">What We Do</h2>
+	<div class="grid grid-cols-1 lg:grid-cols-4 gap-2">
+		<div class="card rounded-none bg-info">
+			<div class="card-body">
+				<h3 class="text-xl">Inspiring Meetups</h3>
+				<p>Exchange ideas, network, and learn from experts.</p>
+			</div>
+		</div>
+		<div class="card rounded-none bg-success">
+			<div class="card-body">
+				<h3 class="text-xl">Technical Workshops</h3>
+				<p>Dive deep into the latest & emerging tools and echnologies.</p>
+			</div>
+		</div>
+		<div class="card rounded-none bg-warning">
+			<div class="card-body">
+				<h3 class="text-xl">Skill development workshops</h3>
+				<p>
+					Learn essential soft skills, including project management and effective communication in
+					tech environments
+				</p>
+			</div>
+		</div>
+		<div class="card rounded-none bg-error">
+			<div class="card-body">
+				<h3 class="text-xl">Networking events</h3>
+				<p>Built relationships & get to know others in the local tech community.</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container mx-auto p-10">
+	<h2 class="text-3xl font-bold mb-5">Upcoming Events</h2>
+	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+		<div class="card rounded-none bg-base-300 dark:bg-base-800">
+			<div class="card-body">
+				<h2
+					class="card-title
+					"
+				>
+					Build with AI
+				</h2>
+				<p>Date TBC.</p>
+				<a href="/" class="btn btn-primary">Learn More</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="bg-neutral">
+	<div class="container mx-auto p-10">
+		<h2 class="text-3xl font-bold mb-5 text-neutral-content">Partners</h2>
+		<div class="grid grid-cols-2 lg:grid-cols-4 gap-5">
+			<div class="hidden lg:block" />
+			<img src="/images/frs.png" alt="FRS Recruitment Logo" class="h-20" />
+			<img src="/images/midlands-ireland.png" alt="Midlands Ireland Logo" class="h-20" />
+		</div>
+	</div>
+</div>
