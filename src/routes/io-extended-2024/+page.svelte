d894292363d1a577b111283e2133e22093fd0d1e
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2024-05-09 09:39:07 +0100

    wip: io extended 2024

---
+++ src/routes/io-extended-2024/+page.svelte
@@ -0,0 +1,34 @@
+<div class="w-full h-auto hidden md:flex">
+	<img src="/images/IOExtended2024-Website-Banner-B.png" alt="IO Extended 2024" />
+</div>
+<div class="w-full h-auto flex md:hidden">
+	<img
+		src="/images/IOExtended2024-Website-Banner-B-Mobile.png"
+		alt="IO Extended 2024"
+		class="mx-auto"
+	/>
+</div>
+<div class="container mx-auto my-24 px-6 lg:px-0">
+	<h1 class="text-5xl font-bold">I/O Extended 2024</h1>
+	<h2 class="text-3xl font-bold mt-4">12th July 2024, 14:00 - 18:30</h2>
+	<p class="mt-4 w-2/3">
+		I/O Extended 2024 is a community-led event hosted by GDG Portlaoise. Join us for a gathering of
+		developers and tech enthusiasts, where we can learn, share, and connect with each other. The
+		event will feature talks, workshops, and networking sessions.
+	</p>
+</div>
+<div class="bg-[url('images/IOExtended24-Bevy-chaper-banner-2560x650.png')] bg-cover bg-center">
+	<div class="container mx-auto py-16 px-6 lg:px-0">
+		<h2 class="text-3xl font-bold">A title here</h2>
+		<p class="mt-4 w-1/2">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris.
+		</p>
+		<a
+			href="https://gdgportlaoise.com"
+			target="_blank"
+			class="mt-4 btn btn-primary rounded-md text-white">CTA Here</a
+		>
+	</div>
+</div>
