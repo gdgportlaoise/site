447f57080f2c229365d56778955833dae6c1fa62
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2024-05-17 10:02:46 +0100

    feat: survey page

---
+++ src/routes/survey/+page.svelte
@@ -0,0 +1,8 @@
+<iframe
+	title="survey"
+	src="https://docs.google.com/forms/d/e/1FAIpQLSfKcrpQ2SYd1p3eYmctCDlxo0PIu1R5odI8q8IjYFAPdnxkMQ/viewform?embedded=true"
+	class="w-full h-[2560px]"
+	frameborder="0"
+	marginheight="0"
+	marginwidth="0">Loading…</iframe
+>
